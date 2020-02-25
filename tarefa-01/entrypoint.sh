1c8cd47e74efc8003f78c3155355526a30458588
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2020-02-25 20:49:48 -0300

    Migração da chamada do dockerise para dentro dos arquivos Dockerfile

--- tarefa-01/entrypoint.sh
+++ tarefa-01/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+chown -R www-data:www-data /var/www/storage
+chmod -R 755 /var/www/storage
+
 composer install
 php artisan key:generate
+php artisan config:cache
 php artisan migrate
 php-fpm
