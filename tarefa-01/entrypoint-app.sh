90e67572b65615bb25694346a7bbccf08938f20c
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2020-02-26 10:27:35 -0300

    Migração de toda a execução das aplicações para dentro de scripts .sh e inclusão da referência nos arquivos de construção Dockerfile

---
+++ tarefa-01/entrypoint-app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dockerize -template ./env.tmpl:./.env
+chown -R www-data:www-data /var/www/storage
+chmod -R 755 /var/www/storage
+
+composer install
+php artisan key:generate
+php artisan config:cache
+php artisan migrate
+php-fpm
